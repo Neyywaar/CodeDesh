d4b2c78c8f191b3318885b616fa8cc60a7e612c7
Sandesh Shrestha <126408282+Neyywaar@users.noreply.github.com>
2023-03-14 11:12:42 +0545

    Add files via upload

---
+++ JS/jquery-linedtextarea.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $(".lined").linedtextarea();
+ 
+ });
+
+ $(".lined").linedtextarea({
+    selectedLine: 4
+ });
--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,40 @@ function run(){
     output.contentDocument.body.innerHTML = htmlCode+"<style>"+cssCode+"</style>";
     output.contentWindow.eval(jsCode);
 }
+
+// Get all textareas on the page
+const textareas = document.querySelectorAll('textarea');
+
+// Loop through each textarea
+textareas.forEach((textarea) => {
+  // Apply the desired action to each textarea
+  // For example, add a tab indent function
+  textarea.addEventListener('keydown', (e) => {
+    if (e.keyCode === 9) {
+      e.preventDefault();
+
+      textarea.setRangeText(
+        '  ',
+        textarea.selectionStart,
+        textarea.selectionStart,
+        'end'
+      )
+    }
+  })
+});
+
+//For Language Tabs
+function codeLanguages(evt, language) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(language).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
